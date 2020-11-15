042a719f1bf979291d1760cbbd122ba39dd8b810
Min <minmusxminmus@hotmail.com>
2020-11-15 18:01:25 +0100

    Added a unit test for AppMusic

--- AppMusic/src/main/java/tds/AppMusic/AppMusic.java
+++ AppMusic/src/main/java/tds/AppMusic/AppMusic.java
@@ -1,4 +1,7 @@
 package tds.AppMusic;
 
 public class AppMusic {
+    public static void main(String[] args) {
+        System.out.println("No functionality yet!");
+    }
 }
---
+++ AppMusic/src/test/java/tds/AppMusic/AppMusicTest.java
@@ -0,0 +1,9 @@
+package tds.AppMusic;
+
+import junit.framework.TestCase;
+
+public class AppMusicTest extends TestCase {
+
+    public void testMain() {
+    }
+}

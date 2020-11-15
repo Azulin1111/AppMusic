e25ff021a12cd8c86e5b568642609ae3c06c66f4
Usuario <Usuario@DESKTOP-DM9FNF8>
2020-11-15 17:11:44 +0100

    primer commit

---
+++ AppMusic/src/main/java/tds/AppMusic/App.java
@@ -0,0 +1,13 @@
+package tds.AppMusic;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

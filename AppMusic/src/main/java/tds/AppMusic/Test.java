a7c8d74f9ae19cd2ca91e88706d2241132062060
Min <minmusxminmus@hotmail.com>
2020-11-15 17:45:14 +0100

    He vuelto a cambiar el test

--- AppMusic/src/main/java/tds/AppMusic/Test.java
+++ AppMusic/src/main/java/tds/AppMusic/Test.java
@@ -2,6 +2,6 @@ package tds.AppMusic;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Ekam holo");
+        System.out.println("Sergio holo");
     }
 }

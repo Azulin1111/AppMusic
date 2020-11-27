e0e34a64132faece6e14c062ff579b237a694ef0
Min <minmusxminmus@hotmail.com>
2020-11-27 17:59:16 +0100

    Implemented Persistence Server: started testing

--- AppMusic/src/main/java/tds/AppMusic/AppMusic.java
+++ AppMusic/src/main/java/tds/AppMusic/AppMusic.java
@@ -1,8 +1,22 @@
 package tds.AppMusic;
 
+import beans.Entidad;
+import tds.driver.FactoriaServicioPersistencia;
+import tds.driver.ServicioPersistencia;
+
+import java.util.List;
+
 public class AppMusic {
     public static void main(String[] args) {
         System.out.println("No functionality yet!");
         // TODO app launch, GUI launch, etc.
+
+        // TESTING
+
+        ServicioPersistencia sp = FactoriaServicioPersistencia.getInstance().getServicioPersistencia();
+        System.out.println("Cosa insertada. Recuperandola...");
+
+        List<Entidad> l = sp.recuperarEntidades();
+        l.forEach(en -> System.out.println(en.getNombre()));
     }
 }

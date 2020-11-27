7fd9c09630876569e45fc038ed96515a93357d32
Min <minmusxminmus@hotmail.com>
2020-11-27 17:06:28 +0100

    Added resources folder.

--- AppMusic/src/main/java/tds/AppMusic/AppMusic.java
+++ AppMusic/src/main/java/tds/AppMusic/AppMusic.java
@@ -3,5 +3,6 @@ package tds.AppMusic;
 public class AppMusic {
     public static void main(String[] args) {
         System.out.println("No functionality yet!");
+        // TODO app launch, GUI launch, etc.
     }
 }

68caac7bd8c3429effa4ab9a3781d2e9ec50df63
azulin1111 <sergiorequenamartinez@gmail.com>
2021-02-19 17:04:45 +0100

    Principal View: -Some things of the principal view.

---
+++ AppMusic/src/main/java/tds/AppMusic/GUI/Launcher.java
@@ -0,0 +1,7 @@
+package tds.AppMusic.GUI;
+
+public class Launcher { // TODO No es una clase final, es solo para lanzar las ventanas
+    public static void main(String[] args) {
+        PrincipalView principalView = new PrincipalView();
+    }
+}

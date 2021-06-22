d97d026f1f9232f7029071a6d5238ca1540c8c07
azulin1111 <sergiorequenamartinez@gmail.com>
2021-06-22 17:09:50 +0200

    AppMusic mostrar ventana

--- AppMusic/src/main/java/tds/AppMusic/AppMusic.java
+++ AppMusic/src/main/java/tds/AppMusic/AppMusic.java
@@ -1,13 +1,18 @@
 package tds.AppMusic;
 
 import beans.Entidad;
+import tds.AppMusic.GUI.LoginWindow;
 import tds.driver.FactoriaServicioPersistencia;
 import tds.driver.ServicioPersistencia;
 
+import javax.swing.*;
 import java.util.List;
 
 public class AppMusic {
     public static void main(String[] args) {
-        System.out.println("No functionality yet!");
+        JFrame frame = new LoginWindow();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
     }
 }

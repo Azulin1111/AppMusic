84adb3c1922b020ed34c994d37e3e7f6f8299e30
Min <minmusxminmus@hotmail.com>
2021-06-23 16:25:26 +0200

    Javadoc + tests

--- AppMusic/src/main/java/tds/AppMusic/model/music/Song.java
+++ AppMusic/src/main/java/tds/AppMusic/model/music/Song.java
@@ -1,25 +1,61 @@
 package tds.AppMusic.model.music;
 
-// TODO add documentation
+import java.net.URI;
+
+/**
+ * Representa una canción.
+ * @author Ekam Puri Nieto
+ * @author Sergio Requena Martínez
+ * @author ekam.purin@um.es
+ * @author sergio.requenam@um.es
+ */
 public class Song {
 
+    /**
+     * El nombre de la canción.
+     */
     private final String name;
+    /**
+     * El género de la canción.
+     */
     private final Genre genre;
-    private final String path;
+    /**
+     * La ruta al fichero que contiene la canción.
+     */
+    private final URI path;
+    /**
+     * El intérprete de la canción.
+     */
     private final String singer;
-    private int playCount = 0;
+    /**
+     * El número de reproducciones de la canción.
+     */
+    private int playCount;
+    /**
+     * El código único de la canción.
+     */
     private int code;
 
-    public Song(String name, String singer, Genre genre, String path) {
-        this.name = name;
-        this.singer = singer;
-        this.genre = genre;
-        this.path = path;
-        code = 0;
-
+    /**
+     * Crea una canción nueva, es decir, sin reproducciones.
+     * @param name El nombre de la canción.
+     * @param singer El intérprete de la canción.
+     * @param genre El género de la canción.
+     * @param path La ruta al fichero de la canción.
+     */
+    public Song(String name, String singer, Genre genre, URI path) {
+        this(name, singer, genre, path, 0);
     }
 
-    public Song(String name, String singer, Genre genre, String path, int playCount) {
+    /**
+     * Crea una canción.
+     * @param name El nombre de la canción.
+     * @param singer El intérprete de la canción.
+     * @param genre El género de la canción.
+     * @param path La ruta al fichero de la canción.
+     * @param playCount El número de reproducciones de la canción.
+     */
+    public Song(String name, String singer, Genre genre, URI path, int playCount) {
         this.name = name;
         this.genre = genre;
         this.singer = singer;
@@ -27,32 +63,50 @@ public class Song {
         this.playCount = playCount;
     }
 
+    /**
+     * Devuelve el nombre de la canción.
+     * @return El nombre de la canción.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Devuelve el género de la canción.
+     * @return El género de la canción.
+     */
     public Genre getGenre() {
         return genre;
     }
 
+    /**
+     * Devuelve el intérprete de la canción.
+     * @return El intérprete de la canción.
+     */
     public String getSinger(){
         return singer;
     }
 
-    public String getPath() {
+    /**
+     * Devuelve la ruta al fichero de la canción.
+     * @return La ruta al fichero de la canción.
+     */
+    public URI getPath() {
         return path;
     }
 
+    /**
+     * Devuelve el número de reproducciones de la canción.
+     * @return el número de reproducciones de la canción.
+     */
     public int getPlayCount() {
         return playCount;
     }
 
+    /**
+     * Incrementa el número de reproducciones de la canción en 1.
+     */
     public void addPlay() {
         playCount++;
-    }
-
-    @Override
-    public String toString() {
-        return name + ": " + singer;
     }
 }
---
+++ AppMusic/src/test/java/tds/AppMusic/model/music/SongTest.java
@@ -0,0 +1,60 @@
+package tds.AppMusic.model.music;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.net.URI;
+
+import static org.junit.Assert.*;
+
+public class SongTest {
+
+    private static final String SONG_NAME = "Song name";
+    private static final Genre SONG_GENRE = Genre.DB;
+    private static final String SONG_SINGER = "Song singer";
+    private static final URI SONG_PATH = null;
+    private Song song;
+
+    @Before
+    public void setUp() {
+        song = new Song(SONG_NAME, SONG_SINGER, SONG_GENRE, SONG_PATH);
+    }
+
+    @Test
+    public void getName() {
+        // Assert that the name is set correctly
+        assertSame(SONG_NAME, song.getName());
+    }
+
+    @Test
+    public void getGenre() {
+        // Assert that the name is set correctly
+        assertSame(SONG_GENRE, song.getGenre());
+    }
+
+    @Test
+    public void getSinger() {
+        // Assert that the name is set correctly
+        assertSame(SONG_SINGER, song.getSinger());
+    }
+
+    @Test
+    public void getPath() {
+        // Assert that the name is set correctly
+        assertSame(SONG_PATH, song.getPath());
+    }
+
+    @Test
+    public void getPlayCount() {
+        // Assert that the name is set correctly
+        assertSame(0, song.getPlayCount());
+    }
+
+    @Test
+    public void addPlay() {
+        // Assert that the play count updates correctly
+        assertSame(0, song.getPlayCount());
+        song.addPlay();
+        assertSame(1, song.getPlayCount());
+    }
+}

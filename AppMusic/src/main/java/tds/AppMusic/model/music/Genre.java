77f8775d7f4966cae623f3ef580645bd9b67a317
Min <minmusxminmus@hotmail.com>
2021-06-23 14:18:40 +0200

    Javadoc

--- AppMusic/src/main/java/tds/AppMusic/model/music/Genre.java
+++ AppMusic/src/main/java/tds/AppMusic/model/music/Genre.java
@@ -1,5 +1,14 @@
 package tds.AppMusic.model.music;
 
+/**
+ * El género de cada canción individual.
+ * @author Ekam Puri Nieto
+ * @author Sergio Requena Martínez
+ * @author ekam.purin@um.es
+ * @author sergio.requenam@um.es
+ */
 public enum Genre {
-    LOFI, ROCK, COUNTRY, DB, DISCO, JAZZ, HIPHOP, REGGAE, DUBSTEP, REGGAETON, VAPORWAVE, SWING, HOUSE, TRAP, EDM, TECHNO, TRANCE, GARAGE, CHIPTUNE, FLAMENCO, FOLK, POP, RB, METAL, PUNK
+    LOFI, ROCK, COUNTRY, DB, DISCO, JAZZ, HIPHOP, REGGAE, DUBSTEP, REGGAETON,
+    VAPORWAVE, SWING, HOUSE, TRAP, EDM, TECHNO, TRANCE, GARAGE, CHIPTUNE,
+    FLAMENCO, FOLK, POP, RB, METAL, PUNK
 }

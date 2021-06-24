a3aea0fa9bdffcfc4057cad91cfbb2eebfea8f35
Min <minmusxminmus@hotmail.com>
2021-06-24 17:57:23 +0200

    Javadoc + tests

--- AppMusic/src/test/java/tds/AppMusic/model/users/UserTest.java
+++ AppMusic/src/test/java/tds/AppMusic/model/users/UserTest.java
@@ -9,6 +9,7 @@ import tds.AppMusic.model.music.Song;
 
 import java.time.Instant;
 import java.time.LocalDate;
+import java.util.Collections;
 import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
@@ -20,6 +21,13 @@ public class UserTest {
     User user;
     Date date = Date.from(Instant.now());
     private static Song s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12;
+
+    private static final String TEST_NAME = "Test name";
+    private static final String TEST_SURNAMES = "Test surnames";
+    private static final boolean TEST_PREMIUM = false;
+    private static final String TEST_NICKNAME = "Test nickname";
+    private static final String TEST_PASSWORD = "Test password";
+    private static final String TEST_EMAIL = "Test email";
 
     @BeforeClass
     public static void beforeAll() {
@@ -39,23 +47,22 @@ public class UserTest {
 
     @Before
     public void setUp() {
-        user = new User("Test name", "Test nickname", false, "Test password", "Test email", date);
+        user = new User(TEST_NAME, TEST_NICKNAME, TEST_PREMIUM, TEST_PASSWORD, TEST_EMAIL, date);
     }
-
 
 
     @Test
     public void parameterTests() {
-        assertEquals("Test name", user.getName());
-        assertEquals("Test nickname", user.getNickname());
-        assertFalse(user.isPremium());
-        assertEquals("Test password", user.getPassword());
-        assertEquals("Test email", user.getEmail());
+        assertEquals(TEST_NAME, user.getName());
+        assertEquals(TEST_NICKNAME, user.getNickname());
+        assertFalse(user.isPremium() ^ TEST_PREMIUM);
+        assertEquals(TEST_PASSWORD, user.getPassword());
+        assertEquals(TEST_EMAIL, user.getEmail());
         assertSame(date, user.getBirthday());
     }
 
     @Test
-    public void fixedDiscountTest(){ //TODO probar estos tests
+    public void fixedDiscountTest() {
         double payment = user.premiumPayment("FixedDiscount");
 
         // Expected payment
@@ -67,7 +74,7 @@ public class UserTest {
     }
 
     @Test
-    public void youngDiscountTest(){
+    public void youngDiscountTest() {
         double payment = user.premiumPayment("YoungDiscount");
 
         // Expected payment
@@ -78,7 +85,7 @@ public class UserTest {
     }
 
     @Test
-    public void nullDiscountTest1(){ // Explicit form
+    public void nullDiscountTest1() { // Explicit form
         double payment1 = user.premiumPayment("ninguno");
 
         // Expected payment
@@ -89,7 +96,7 @@ public class UserTest {
     }
 
     @Test
-    public void nullDiscountTest2(){ // Implicit form
+    public void nullDiscountTest2() { // Implicit form
         double payment2 = user.premiumPayment("NullDiscount");
 
         // Expected payment
@@ -100,24 +107,22 @@ public class UserTest {
     }
 
     @Test
-    public void mostPlayedSongsTest1(){
-        Playlist test = user.createPlaylist("test");
-
-        test.addSong(s1); test.addSong(s2); test.addSong(s3); test.addSong(s4); test.addSong(s5); test.addSong(s6);
-        test.addSong(s7); test.addSong(s8); test.addSong(s9); test.addSong(s10); test.addSong(s11); test.addSong(s12);
+    public void mostPlayedSongsTest1() {
+        List<Song> songs = new LinkedList<>();
+        Collections.addAll(songs, s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12);
+        Playlist test = user.createPlaylist("test", songs);
         List<Song> mostPlayedSongs = user.getMostPlayedSongs();
 
         // Expected
         List<Song> expected = new LinkedList<>();
-        expected.add(s12); expected.add(s11); expected.add(s10); expected.add(s9); expected.add(s8);
-        expected.add(s7); expected.add(s6); expected.add(s5); expected.add(s4); expected.add(s3);
+        Collections.addAll(expected, s12, s11, s10, s9, s8, s7, s6, s5, s4, s3);
 
         // Test
         assertEquals(mostPlayedSongs, expected);
     }
 
     @Test
-    public void mostPlayedSongsTest2(){
+    public void mostPlayedSongsTest2() {
         Playlist p1 = user.createPlaylist("p1");
         Playlist p2 = user.createPlaylist("p2");
 
@@ -136,7 +141,7 @@ public class UserTest {
     }
 
     @Test
-    public void addRecentSongTest(){
+    public void addRecentSongTest() {
         user.addRecentSong(s1); user.addRecentSong(s2); user.addRecentSong(s3); user.addRecentSong(s4);
         user.addRecentSong(s5); user.addRecentSong(s6); user.addRecentSong(s7); user.addRecentSong(s8);
         user.addRecentSong(s9); user.addRecentSong(s10); user.addRecentSong(s11); user.addRecentSong(s12);
@@ -150,10 +155,5 @@ public class UserTest {
 
         // Test
         assertEquals(recentSongs, expected);
-
-
-
     }
-
-
 }

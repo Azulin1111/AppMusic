a9f2cc293252cad8500ca5f650cbe7472f5ee9bb
Min <minmusxminmus@hotmail.com>
2021-06-24 21:18:09 +0200

    Fichero de pruebas creado

---
+++ AppMusic/src/test/java/tds/AppMusic/persistance/AdaptadorPlaylistDAOTest.java
@@ -0,0 +1,38 @@
+package tds.AppMusic.persistance;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AdaptadorPlaylistDAOTest {
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void storePlaylist() {
+    }
+
+    @Test
+    public void deletePlaylist() {
+    }
+
+    @Test
+    public void setPlaylist() {
+    }
+
+    @Test
+    public void getPlaylist() {
+    }
+
+    @Test
+    public void getAllPlaylists() {
+    }
+}

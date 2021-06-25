d4c85e12eaf876de065ef47328d8c5a318afe215
Min <minmusxminmus@hotmail.com>
2021-06-25 17:43:52 +0200

    Fix notacion

--- AppMusic/src/test/java/tds/AppMusic/model/discount/DiscountTest.java
+++ AppMusic/src/test/java/tds/AppMusic/model/discount/DiscountTest.java
@@ -7,9 +7,9 @@ import tds.AppMusic.model.users.User;
 import static org.junit.Assert.*;
 
 public class DiscountTest {
-    double testPrize;
-    Discount discount1 = new FixedDiscount();
-    Discount discount2 = new YoungDiscount();
+    private static double testPrize;
+    private static final Discount DISCOUNT_1 = new FixedDiscount();
+    private static final Discount DISCOUNT_2 = new YoungDiscount();
 
     @Before
     public void setUp() {
@@ -19,12 +19,12 @@ public class DiscountTest {
     @Test
     public void fixedDiscountTest() {
         double expected = User.PREMIUM_PRIZE * 0.7;
-        assertEquals(expected, discount1.calcDescuento(), 0.01);
+        assertEquals(expected, DISCOUNT_1.calcDescuento(), 0.01);
     }
 
     @Test
     public void youngDiscountTest() {
         double expected = User.PREMIUM_PRIZE * 0.85;
-        assertEquals(expected, discount2.calcDescuento(), 0.01);
+        assertEquals(expected, DISCOUNT_2.calcDescuento(), 0.01);
     }
 }

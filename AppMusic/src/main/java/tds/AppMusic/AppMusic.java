53cbba292c4c58e67215bd842dc5f4cf34e73a71
Min <minmusxminmus@hotmail.com>
2021-06-30 15:36:44 +0200

    Main finalizado

--- AppMusic/src/main/java/tds/AppMusic/AppMusic.java
+++ AppMusic/src/main/java/tds/AppMusic/AppMusic.java
@@ -5,17 +5,12 @@
 
 package tds.AppMusic;
 
-import beans.Entidad;
 import tds.AppMusic.GUI.LoginWindow;
 import tds.AppMusic.app.Controller;
-import tds.driver.FactoriaServicioPersistencia;
 import umu.tds.ISongFinder;
 import umu.tds.LoaderSong;
 
 import javax.swing.*;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-import java.util.stream.Collectors;
 
 public class AppMusic {
 
@@ -23,33 +18,15 @@ public class AppMusic {
         ISongFinder finder = new LoaderSong();
         Controller.INSTANCE.setLoader(finder);
 
-//        clearDB();
-        showDB();
-
         // Clean L&F
-        try {
-             UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        } finally {
+        try { UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel"); }
+        catch (Exception e) { e.printStackTrace(); }
+        finally {
             LoginWindow lw = new LoginWindow();
             lw.pack();
             lw.setLocationRelativeTo(null);
             lw.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
             lw.setVisible(true);
         }
-    }
-
-    public static void clearDB() {
-        FactoriaServicioPersistencia.getInstance().getServicioPersistencia().recuperarEntidades().forEach(e -> {
-            System.out.println("Borrando " + e.getNombre() + e.getPropiedades().stream().map(p -> p.getNombre() + ": " + p.getValor()).collect(Collectors.toList()));
-            FactoriaServicioPersistencia.getInstance().getServicioPersistencia().borrarEntidad(e);
-        });
-    }
-
-    public static void showDB() {
-        for (Entidad e : FactoriaServicioPersistencia.getInstance().getServicioPersistencia().recuperarEntidades())
-            System.out.println(e.getNombre() + e.getPropiedades().stream().map(p -> p.getNombre() + ": " + p.getValor() + "\n\t").collect(Collectors.toList()) + "\n\n");
     }
 }
